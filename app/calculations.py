70004cc646d1a8dbfc58699c61e6acffe93f527f
jayapat <pjay271024@google.com>
2023-07-05 14:03:47 -0700

    few changes in test made

--- app/calculations.py
+++ app/calculations.py
@@ -1,2 +1,29 @@
 def add(num1: int, num2: 2):
     return num1 + num2
+
+def multi(num1: int, num2: 2):
+    return num1 * num2 
+
+def div(num1: int, num2: 2):
+    return num1 / num2
+
+def min(num1: int, num2: 2):
+    return num1 - num2
+
+class Insufficentfund(Exception):
+    pass
+
+class BankAccount():
+    def __init__(self, starting_balance = 0):
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise Insufficentfund("balance is insufficient")
+        self.balance -= amount
+
+    def collect_interest(self, amount):
+        self.balance *= 1.1
--- test/test_calculations.py
+++ test/test_calculations.py
@@ -1,6 +1,48 @@
-from app.calculations import add
+import pytest
+from app.calculations import add, multi, min, div, BankAccount, Insufficentfund
+@pytest.fixture
+def zero_bankaccount():
+    return BankAccount()
 
-def test_add():
-    print("testing add function")
-    assert 8 == add(5,3)
-test_add()
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+@pytest.mark.parametrize("x, y, z", [
+    (5,3,8), (3,2,5), (5,-6,-1), (5,-5,0)
+])
+def test_add(x, y, z):
+    assert add(x,y) == z
+
+def test_min():
+    assert min(5,3) ==2
+
+def test_multi():
+    assert multi(5,3) == 15
+
+def test_div():
+    assert div(6,3) == 2
+
+def test_bank_set_initial_amount(zero_bankaccount):
+    assert zero_bankaccount.balance == 0
+
+def test_bank_default_amount(bank_account):
+    bank_account.deposit(10)
+    assert bank_account.balance  == 60
+
+@pytest.mark.parametrize("dipo, withd, balance", [
+    (50,30,20), (100,80,20)
+])
+def test_bank_trasactions(zero_bankaccount, dipo, withd, balance):
+    zero_bankaccount.deposit(dipo)
+    zero_bankaccount.withdraw(withd)
+    assert zero_bankaccount.balance == balance
+
+@pytest.mark.parametrize("dipo, withd, balance", [
+    (50,200,20)
+])
+def test_bank_trasactions_expection(zero_bankaccount, dipo, withd, balance):
+    with pytest.raises(Insufficentfund):
+        zero_bankaccount.deposit(dipo)
+        zero_bankaccount.withdraw(withd)
+        assert zero_bankaccount.balance == balance
